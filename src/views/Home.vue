55f6503d7ad0c09d461e7366b52775f744db9be5
TanyaGashtold <tanya@form.io>
2022-01-29 23:24:02 +0300

    to do done

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,93 +5,146 @@
         <h3 class="mb-5">TO DO LIST</h3>
       </div>
       <div class="d-flex justify-content-center mb-4">
-        <input type="text" :style="{width:'100%', marginRight:'1rem', padding: '5px 10px'}" placeholder="Enter ToDo">
-        <button type="button" class="btn btn-primary">Add</button>
+        <input type="text" v-model="newToDo" :style="{width:'100%', marginRight:'1rem', padding: '5px 10px'}" placeholder="Enter ToDo">
+        <button type="button" class="btn btn-primary" @click="addToDo">Add</button>
       </div>
-      <input type="text" :style="{width:'100%',  padding: '5px 10px'}" class="mb-3" placeholder="Rearch">
-      <ul class="list-group">
-        <li class="list-group-item">
-          <div class="d-flex  justify-content-between">
-            <p>clean room</p>
+      <input type="text" :style="{width:'100%',  padding: '5px 10px'}" class="mb-3"  v-model="query" placeholder="Rearch">
+      <ul class="list-group" v-if="filteredToDoes.length">
+        <li v-for="todo in pageToDoes" class="list-group-item" :key="todo.id">
+          <div v-if="!underEditing(todo.id)" class="d-flex  justify-content-between">
+            <p v-if="!todo.done">{{todo.toDo}}</p>
+            <p v-if="todo.done"><del>{{todo.toDo}}</del></p>
+            <div class="btn-group"  role="group" aria-label="Basic example">
+              <button type="button"  @click="toggleToDo(todo.id)" class="btn btn-success">{{todo.done ? 'Undone': 'Done'}}</button>
+              <button type="button" class="btn btn-primary" @click="editToDo(todo.id)">Edit</button>
+              <button type="button" class="btn btn-danger" @click="deleteToDo(todo.id)">Delete</button>
+            </div>
+          </div>
+          <div v-else class="d-flex  justify-content-between">
+            <input type="text" v-model="todo.toDo" :style="{width:'100%', marginRight:'1rem', padding: '5px 10px'}" placeholder="ToDo">
             <div class="btn-group" role="group" aria-label="Basic example">
-              <button type="button" class="btn btn-success">Done</button>
-              <button type="button" class="btn btn-primary">Edit</button>
-              <button type="button" class="btn btn-danger">Delete</button>
+              <button type="button" class="btn btn-primary" @click="saveToDo(todo.id)">Save</button>
+              <button type="button" class="btn btn-danger" @click="cancelToDo(todo.id)">Cancel</button>
             </div>
           </div>
         </li>
       </ul>
+      <div v-else>
+        <p>No Items In ToDo List</p>
+      </div>
+      <nav aria-label="Page navigation example" v-if="filteredToDoes.length" class="mt-3">
+        <ul class="pagination justify-content-center">
+          <li :class="{'page-item': true, disabled: currentPage === 1}" @click="prevPage"><a class="page-link" href="#">Prev</a></li>
+          <li :class="{'page-item': true, active: page === currentPage}" v-for="page in pagesCount" :key="page" @click="switchPage(page)"><a class="page-link"  >{{page}}</a></li>
+          <li :class="{'page-item': true, disabled: currentPage === pagesCount}" @click="nextPage"><a class="page-link" href="#">Next</a></li>
+        </ul>
+      </nav>
     </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
-const API_URL = 'https://my-json-server.typicode.com/tgashtold/json-server';
-const requestUrls = {
-  db: API_URL +'/db',
-  todos: API_URL +'/posts',
-}
-// const defaultToDos = [
-//   {toDo: 'Clean Room', done: false},
-//   {toDo: 'Feed Cat', done: true},
-//   {toDo: 'Find hotel in Giorgia', done: false}
-// ];
+const API_URL = 'https://my-json-server.typicode.com/tgashtold/vue-todo-list/todos';
+const itemsPerPage = 10;
 
 export default {
   name: 'Home',
-  // components: {
-  //   HelloWorld
-  // }
   data: ()=> ({
-    todos: []
+    toDoes: [],
+    newToDo: '',
+    editedToDoes: [],
+    query: '',
+    currentPage: 1
   }),
   mounted: function() {
-    axios.post(requestUrls.todos, {toDo: 'Clean Room', done: false}).then((res) => {
-      console.log(111,res)
-    }).catch((err) => {
-      console.log(err)
-    })
-
- axios.get(requestUrls.todos).then((res) => {
-      console.log(111,res)
+     axios.get(API_URL).then((res) => {
+      this.toDoes = res.data;
     }).catch((err) => {
       console.log(err)
     })
   },
   methods: {
-    // prevEmployee(){
-    //   if(this.display > 0){
-    //     this.display --
-    //   }
-    // },
-    // nextEmployee(){
-    //   if(this.display < (this.employees.length-1)){
-    //     this.display ++
-    //   }
-    // },
-    // showDetails(index) {
-    //   this.display = index;
-    // },
-    // toggleList() {
-    //   this.showList = !this.showList;
-    //   this.display = undefined;
-    // }
+    prevPage() {
+      if(this.currentPage !== 1){
+        this.currentPage = --this.currentPage;
+      }
+    },
+    nextPage() {
+      if (this.currentPage !== this.pagesCount) {
+        this.currentPage = ++this.currentPage;
+      }
+    },
+    switchPage(page) {
+      this.currentPage = page;
+    },
+    addToDo(){
+      const toDo = this.newToDo.trim();
+
+      if (toDo.length) {
+        this.toDoes.unshift({
+          toDo,
+          done:false,
+          id: Math.random() * 10000
+        });
+        this.newToDo = '';
+      }
+    },
+    deleteToDo(id) {
+      this.toDoes = this.toDoes.filter(todo => todo.id !== id);
+      if(this.currentPage>this.pagesCount) {
+        this.currentPage = --this.currentPage;
+      }
+    },
+    toggleToDo(id){
+      this.toDoes = this.toDoes.map(todo => {
+        if(todo.id === id) {
+          todo.done = !todo.done;
+        }
+        return todo;
+        });
+    },
+    editToDo(id){
+      const editedToDo = this.toDoes.find(todo => todo.id === id);
+      this.editedToDoes.push(JSON.parse(JSON.stringify(editedToDo)));
+    },
+    underEditing(id) {
+      return this.editedToDoes.some(todo => todo.id === id);
+    },
+    saveToDo(id) {
+      this.editedToDoes = this.editedToDoes.filter(todo => todo.id !== id);
+      this.currentPage = 1;
+    },
+    cancelToDo(id) {
+      this.editedToDoes = this.editedToDoes.filter(edittodo => {
+        const underEditing = edittodo.id === id;
+        if(underEditing) {
+          this.toDoes = this.toDoes.map(todo => {
+            return todo.id === id ? edittodo : todo;
+          })
+        }
+        
+      })
+    }
   },
-  filters: {
-    // capitalize(name) {
-    //   return name.split(/\s+/).map(part => part.charAt(0).toUpperCase() + part.toLowerCase().slice(1)).join(' ');
-    // } 
-  }
+  computed: {
+    filteredToDoes() {
+      const query = this.query.toLowerCase().trim();
 
-
+      return query.length > 3 
+        ?  this.toDoes.filter(todo => todo.toDo.toLowerCase().includes(query))
+        :  this.toDoes;
+    },
+    pagesCount() {
+      const items = this.filteredToDoes.length;
+      return items ? Math.ceil(items/itemsPerPage) : null;
+    },
+    pageToDoes() {
+      return this.filteredToDoes.slice((this.currentPage-1)*itemsPerPage, this.currentPage*itemsPerPage);
+    }
+  },
 }
 </script>
 
 <style scoped lang="scss">
-// .card {
-//   border: 2px green solid;
-//   margin-top: 10px;
-//   padding: 1rem;
-// }
 </style>
